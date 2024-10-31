6d6aef59a463d0fe4ccc5bf455ad908e7b4cfd4d
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2024-10-31 10:41:23 +0800

    Create Test++.java

---
+++ Test++.java
@@ -0,0 +1,24 @@
+package com.example.myapplication2_2_1;
+
+public class Test{
+
+    // 主程序
+    public static void main(String[] args) {
+        int a=2;
+        int b=(a++)*3;
+        int c=(++a)*3;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        }
+}
+
+*****************************************************
+4
+6
+12
+
+进程已结束，退出代码为 0
