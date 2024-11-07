5fe18ff544f0250f7bbe7400724ed9551e591dfb
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2024-11-07 10:07:15 +0800

    Create array_storage_allocation.java

---
+++ array_storage_allocation.java
@@ -0,0 +1,25 @@
+package com.example.myapplication2_2_1;
+
+public class Test{
+
+    // 主程序
+    public static void main(String[] args) {
+
+
+
+        int a=2;
+        int b=(a++)*3;
+        int c=(++a)*3;
+
+        String s1=new String("abc");
+        String s2="abc";
+
+
+        
+        System.out.println(s1==s2);
+
+        }
+}
+
+***************************************************
+  false
