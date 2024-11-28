715ceb1aa65521131343e01ce9146809f0beda0a
Aureole-zcg <85923439+Aureole-zcg@users.noreply.github.com>
2024-11-28 11:46:48 +0800

    Update MainActivity.java

--- temperature switch/MainActivity.java
+++ temperature switch/MainActivity.java
@@ -1,3 +1,4 @@
+package experiment1.ZhahngChenguang;
 
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {//活动建立调用此方法
+        super.onCreate(savedInstanceState);//调用父类的onCreate()
 //        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//设置Activity使用的布局资源文件
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.button), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,13 +26,12 @@ public class MainActivity extends AppCompatActivity {
     //button控件的事件处理办法
     public void button_Click(View view)
     {
-        int c;
-        double f;
+        double c,f;
         //取得EditText控件
         EditText txtC=(EditText) findViewById(R.id.txtC);
         EditText txtF=(EditText) findViewById(R.id.txtF);
         //取得输入式
-        c=Integer.parseInt(txtC.getText().toString());
+        c=Double.parseDouble(txtC.getText().toString());
         //摄氏度换算华氏度公式
         f=(9.0*c)/5.0+32.0;
         txtF.setText(String.valueOf(f));
